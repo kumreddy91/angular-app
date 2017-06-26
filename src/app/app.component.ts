656d72992ee01c2d9d165f5fc73dafa0f52efecc
ukukkala <ukukkala@gmail.com>
2017-06-26 01:21:28 -0500

    app component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,7 +5,7 @@ import { Component } from '@angular/core';
   template: `
     <ul>
       <li><a routerLink="/">Home</a></li>
-      <li><a routerLink="/about">About</a></li>
+      <li><a routerLink="/about">About</a></li> 
     </ul>
     <router-outlet></router-outlet>
   `,
